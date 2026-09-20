61d7919dbb8c8c7c69245f2ea93c0d6a9cc77518
rakayriii <rakayriii@gmail.com>
2026-09-20 13:38:15 +0700

    fix: configure Vercel Docker deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  output: "standalone",
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "50mb",
+    },
+  },
 };
 
 export default nextConfig;
